ec64b3ba8c6e094511e7704b9700acac9390b6d9
Michael Kummer <mkummer@ualberta.ca>
2025-03-08 23:44:39 +0000

    added math formatting

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741475419',resources: [
+    cacheName: 'chirpy-1741477444',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
