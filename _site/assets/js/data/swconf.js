6328fe34b84abb512948af5c158ef6f44ab5e530
Michael Kummer <mkummer@ualberta.ca>
2025-03-09 00:58:59 +0000

    improved formatting

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741477444',resources: [
+    cacheName: 'chirpy-1741481901',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
