f8a1eb638cae00bc85b3c4cce9be9b6e06f035b6
Michael Kummer <mkummer@ualberta.ca>
2025-04-18 20:18:21 +0000

    Final Fraud Post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744339356',resources: [
+    cacheName: 'chirpy-1745007439',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
