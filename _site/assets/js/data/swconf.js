8d55e650daad63edabfcc2f557cae2eac44e242e
Michael Kummer <mkummer@ualberta.ca>
2025-05-06 00:10:46 +0000

    updated VolunteerConnector Data

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1746138663',resources: [
+    cacheName: 'chirpy-1746490201',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
